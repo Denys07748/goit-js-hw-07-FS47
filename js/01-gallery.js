494406077996a2ebe06824fdfcd212365fc01383
Denys07748 <denis07748@gmail.com>
2022-06-07 23:31:38 +0300

    додає завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('div.gallery');
+const imagesMarcup = createGalleryImgMarcup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('afterbegin', imagesMarcup);
+
+galleryContainerEl.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryImgMarcup(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+}
+
+function onGalleryContainerClick(e) {
+    e.preventDefault();
+    
+    const instance = basicLightbox.create(`
+        <img
+        src="${e.target.dataset.source}"/>
+        `);
+
+    galleryContainerEl.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" && basicLightbox.visible()) {
+        instance.close();
+        }
+    });
+
+    if(!e.target.classList.contains('gallery__image')) {
+        return;
+    } else {
+        instance.show();
+    }  
+}
