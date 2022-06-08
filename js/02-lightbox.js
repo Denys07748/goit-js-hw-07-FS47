aa0997b9a55758344ae8763de708efa2e0925141
Denys07748 <denis07748@gmail.com>
2022-06-08 14:37:25 +0300

    додає рендер розмітки в завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('ul.gallery');
+const imagesMarcup = createGalleryImgMarcup(galleryItems);
+
+galleryContainerEl.innerHTML = imagesMarcup;
+
+// galleryContainerEl.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryImgMarcup(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+            <li>
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}" 
+                    />
+                </a>;
+            </li>
+        `})
+        .join('');
+    }
+
